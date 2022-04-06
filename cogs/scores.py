5a60f658c1c2c92c48730c6198e437040a5553eb
EBraceyIV <dbraceyiv@me.com>
2022-04-06 18:04:39 -0400

    Score cog functional with slash commands. Plus, minus, and scoreboard.

--- cogs/scores.py
+++ cogs/scores.py
@@ -1,32 +1,34 @@
+import asyncio
+
 import discord
 from discord.ext import commands
 from discord import app_commands
 import shelve
 import typing
 
-# # Initialize the scoreboard list of all currently scored members
-# scores = shelve.open("plusMinus")
-# scored_members = list(scores.keys())
-# scores.close()
+# Initialize the scoreboard list of all currently scored members
+scores = shelve.open("plusMinus")
+scored_members = list(scores.keys())
+scores.close()
 
 
-# # Member score management function
-# #   action: Add to or subtract from a score, or see what the score is
-# #   member: Which user's score to manage
-# #   amount: What score to add/remove
-# def score_func(action, member, amount):
-#     global scored_members
-#     plus_minus = shelve.open("plusMinus")
-#     if action == "init":
-#         plus_minus[member] = amount
-#     elif action == "add":
-#         plus_minus[member] = plus_minus.get(member) + amount
-#     elif action == "get":
-#         return plus_minus.get(member)
-#     elif action == "subtract":
-#         plus_minus[member] = plus_minus.get(member) - amount
-#     scored_members = list(plus_minus.keys())
-#     plus_minus.close()
+# Member score management function
+#   action: Add to or subtract from a score, or see what the score is
+#   member: Which user's score to manage
+#   amount: What score to add/remove
+def score_func(action, member, amount):
+    global scored_members
+    plus_minus = shelve.open("plusMinus")
+    if action == "init":
+        plus_minus[member] = amount
+    elif action == "add":
+        plus_minus[member] = plus_minus.get(member) + amount
+    elif action == "get":
+        return plus_minus.get(member)
+    elif action == "subtract":
+        plus_minus[member] = plus_minus.get(member) - amount
+    scored_members = list(plus_minus.keys())
+    plus_minus.close()
 
 
 # This is here to use
@@ -39,8 +41,47 @@ class Scores(commands.Cog, name="Scores"):
 
     # Let users add to other user's scores
     @app_commands.command(name="plus", description="Add to a user's score")
-    async def plus(self, interaction: discord.Interaction, member: discord.Member):
-        await interaction.response.send_message(member.display_name + " +1 ")
+    async def plus(self, interaction: discord.Interaction, member: discord.Member, num: typing.Optional[int] = 1):
+        if member.mention == interaction.user.mention:
+            await interaction.response.send_message("Trying to boost your own numbers? Shameful!")
+        else:
+            if member.mention not in scored_members:
+                score_func("init", member.mention, num)
+            else:
+                score_func("add", member.mention, num)
+            await interaction.response.send_message(str(member.display_name) + ' +' + str(num))
+
+    # Let users subtract from other user's scores
+    @app_commands.command(name="minus", description="Subtract from a user's score")
+    async def minus(self, interaction: discord.Interaction, member: discord.Member, num: typing.Optional[int] = 1):
+        if member.mention == interaction.user.mention:
+            await interaction.response.send_message("I mean, if really want to...\n" +
+                                                    member.display_name + " -" + str(num))
+        else:
+            if member.mention not in scored_members:
+                score_func("init", member.mention, -num)
+            else:
+                score_func("subtract", member.mention, num)
+            await interaction.response.send_message(str(member.display_name) + ' -' + str(num))
+
+    # Show a scoreboard from highest to lowest for all users with a score
+    @app_commands.command(name="scoreboard", description="Scoreboard of the highest and lowest scores.")
+    async def score(self, interaction: discord.Interaction):
+        # Initialize scoreboard embed message and embed description
+        embed = discord.Embed(title="Scoreboard")
+        desc = ""
+        score_list = shelve.open("plusMinus")
+        # Sort the current user scores from highest to lowest
+        score_sorted = sorted(score_list.items(), key=lambda x: x[1])
+        # Iterate through the scores and build the embed content
+        for score in score_sorted:
+            # Here "score" is a tuple, containing the user and score, adding each to a new line
+            desc = str(score[0]) + ": " + str(score[1]) + "\n" + desc
+        score_list.close()
+        # Add some flavor text and send the message
+        embed.description = "Here's the current scoreboard. Honestly can't believe these numbers: \n\n" + desc
+        embed.set_footer(text="Be sure to use bb:+ and bb:- to keep our scoreboard up to date.")
+        await interaction.response.send_message(embed=embed)
 
     # Some general error processing for some of the score commands
 
