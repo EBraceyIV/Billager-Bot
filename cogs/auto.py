1119072e821bec3afbb9c5fe5ac87b1ff70b67ee
EBraceyIV <dbraceyiv@me.com>
2022-04-14 16:46:53 -0400

    Callout post now applies a special roll to the recipient, reduced callout sleep

--- cogs/auto.py
+++ cogs/auto.py
@@ -6,6 +6,8 @@ import asyncio
 import shelve
 
 called_out = False
+# callout_role = 964259482014191737
+
 
 
 class Auto(commands.Cog, name="Auto"):
@@ -29,6 +31,7 @@ class Auto(commands.Cog, name="Auto"):
         # Global variable to keep track of whether or not the callout post has been made yet out of the scope of this
         # listener so that it does not trigger multiple times
         global called_out
+        global callout_role
 
         # Do not reply to BB's own messages
         if message.author == self.bot.user:
@@ -47,7 +50,7 @@ class Auto(commands.Cog, name="Auto"):
                 # after setting called_out to True so that no other messages trigger another post during the wait
                 called_out = True
                 print("Callout post inbound.")
-                await asyncio.sleep(120)
+                await asyncio.sleep(30)
 
                 # Sort the current user scores from highest to lowest
                 plus_minus = shelve.open("plusMinus")
@@ -55,10 +58,16 @@ class Auto(commands.Cog, name="Auto"):
                 plus_minus.close()
 
                 # Send our fun little message letting our friend know they should try making better jokes
-                await message.channel.send("This is your weekly Bad Score Callout Post, a public service brought to "
-                                           "you by Billager Bot. \n"
-                                           "This week, " + str(score_sorted[0][0]) + " has the worst score so far. "
-                                           "All the way down at a fat " + str(score_sorted[0][1]) + "!")
+                callout_post = await message.channel.send("This is your weekly Bad Score Callout Post, a public "
+                                                          "service brought to you by Billager Bot. This week, " +
+                                                          str(score_sorted[0][0]) + " has the worst score so far. All "
+                                                          "the way down at a fat " + str(score_sorted[0][1]) + "!")
+                # Derive the called out role by name
+                # TODO: make this more robust later on
+                callout_role = discord.utils.get(message.author.guild.roles, name="Called Out")
+                # Apply the called out role to the user mentioned in the callout post
+                await callout_post.mentions[0].add_roles(callout_role)
+
         else:
             # Reset the called_out status on Saturday
             if called_out and time.weekday() == 5:
