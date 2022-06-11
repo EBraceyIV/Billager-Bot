94f9b0c89a2265c8b5320170e3e2a732a8670e83
EBraceyIV <dbraceyiv@me.com>
2022-06-11 12:40:13 -0400

    Now using modal in poll command

--- cogs/poll.py
+++ cogs/poll.py
@@ -6,21 +6,36 @@ from discord import app_commands
 import typing
 
 
+class PollModal(discord.ui.Modal, title="Build-A-Poll"):
+    # TextInputs to accept the lore title and description, both required
+    poll_prompt = discord.ui.TextInput(label="Poll Prompt:", style=discord.TextStyle.short, required=True)
+    opt1 = discord.ui.TextInput(label="Option 1:", style=discord.TextStyle.short, required=True)
+    opt2 = discord.ui.TextInput(label="Option 2:", style=discord.TextStyle.short, required=True)
+    opt3 = discord.ui.TextInput(label="Option 3:", style=discord.TextStyle.short, required=False)
+    opt4 = discord.ui.TextInput(label="Option 4:", style=discord.TextStyle.short, required=False)
+
+    async def on_submit(self, interaction: discord.Interaction):
+        embed = discord.Embed(title=self.poll_prompt.value)
+        embed.add_field(name="First choice:", value=self.opt1, inline=False)
+        embed.add_field(name="Second choice:", value=self.opt2, inline=False)
+        if self.opt3.value != "":
+            embed.add_field(name="Third choice:", value=self.opt3, inline=False)
+        if self.opt4.value != "":
+            if self.opt3.value == "":
+                embed.add_field(name="Third choice:", value=self.opt4, inline=False)
+            else:
+                embed.add_field(name="Fourth choice", value=self.opt4, inline=False)
+        await interaction.response.send_message(embed=embed)
+
+
 class Poll(commands.Cog, name="Poll"):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     @app_commands.command(name="poll",
                           description="Start a poll! Enter how long it should last and the options to pick from.")
-    @app_commands.describe(duration="How long should the poll be open?", interval="There is a 7 day (168 hour) limit.")
-    async def poll(self, interaction: discord.Interaction,
-                   title: str, description: str,
-                   duration: str, interval: typing.Literal["hours", "days"],
-                   opt1: str, opt2: str, opt3: typing.Optional[str], opt4: typing.Optional[str]):
-        embed = discord.Embed(title=title, description=description)
-        # self.test.start()
-
-        await interaction.response.send_message(embed=embed)
+    async def poll(self, interaction: discord.Interaction):
+        await interaction.response.send_modal(PollModal())
 
     # @tasks.loop()
     # async def test(self):
